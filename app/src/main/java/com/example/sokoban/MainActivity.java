c9ed08151209640cc196c9814e356379c68b5b3b
koenboh <koen.bohmer@live.nl>
2019-04-11 15:17:51 +0200

    test

--- app/src/main/java/com/example/sokoban/MainActivity.java
+++ app/src/main/java/com/example/sokoban/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    hello this works
+    hello this works!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
